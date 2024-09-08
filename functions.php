ae83b2eb1e69e2c20ac07a0f8fc84d3e5535b6b8
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2024-09-08 13:05:58 +0200

    Add custom taxonomy, update dependencies

--- functions.php
+++ functions.php
@@ -11,3 +11,65 @@ if ( ! function_exists( 'animetravel_setup' ) ) :
 	}
 endif;
 add_action( 'after_setup_theme', 'animetravel_setup' );
+
+function animetravel_register_taxonomy() {
+    register_taxonomy(
+        'season',
+        'post',
+        array(
+            'labels' => array (
+                'name'              => 'Season',
+                'singular_name'     => 'season',
+                'search_items'      => 'Search Season',
+                'all_items'         => 'All seasons',
+                'view_item'         => 'View Season',
+                'parent_item'       => 'Parent Season',
+                'parent_item_colon' => 'Parent Season:',
+                'edit_item'         => 'Edit Season',
+                'update_item'       => 'Update Season',
+                'add_new_item'      => 'Add New season',
+                'new_item_name'     => 'New Season Name',
+                'not_found'         => 'No seasons found',
+                'back_to_items'     =>  'Back to Seasons',
+                'menu_name'         =>  'Seasons',
+            ),
+            'hierarchical'          => true,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'season' ),
+            'show_in_rest'          => true,
+        )
+    );
+    register_taxonomy(
+        'location',
+        'post',
+        array(
+           'labels' => array (
+                'name'              => 'Location',
+                'singular_name'     => 'Location',
+                'search_items'      => 'Search Location',
+                'all_items'         => 'All Locations',
+                'view_item'         => 'View Location',
+                'parent_item'       => 'Parent Location',
+                'parent_item_colon' => 'Parent Location:',
+                'edit_item'         => 'Edit Location',
+                'update_item'       => 'Update Location',
+                'add_new_item'      => 'Add New Location',
+                'new_item_name'     => 'New Location Name',
+                'not_found'         => 'No Locations found',
+                'back_to_items'     =>  'Back to Locations',
+                'menu_name'         =>  'Locations',
+            ),
+            'hierarchical'          => true,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'location' ),
+            'show_in_rest'          => true,
+        )
+    );
+}
+add_action(  'init', 'animetravel_register_taxonomy' );
